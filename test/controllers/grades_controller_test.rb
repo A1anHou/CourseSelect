7cbc77bd45b84e225d3dc60b48783bf03a9bfe74
wangdemo <31690963+wangdemo@users.noreply.github.com>
2019-12-12 21:31:37 +0800

    Add files via upload
    修改分数

--- test/controllers/grades_controller_test.rb
+++ test/controllers/grades_controller_test.rb
@@ -4,4 +4,26 @@ class GradesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+=begin
+  test "index" do
+    #@user = User.new(user_params)
+    @course = Course.new
+    @user = User.new
+    if @course.save||@user.save
+      assert_nil( User)
+      assert_nil( Course )
+    else
+      assert_not_nil( User)
+      assert_not_nil( Course)
+    end
+   end
+=end
+
+  test "grade update?" do
+
+    grade=Grade.new
+    assert grade.save
+    #assert_redirected_to grades_path
+   end
 end
